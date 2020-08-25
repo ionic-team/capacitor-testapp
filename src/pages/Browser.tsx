90513ceff4e42bb9471116323ab0b1a987062cdf
Ian Keith <iankeith@gmail.com>
2020-08-25 12:34:04 -0400

    Updating browser page.

--- src/pages/Browser.tsx
+++ src/pages/Browser.tsx
@@ -6,13 +6,52 @@ import {
   IonMenuButton,
   IonTitle,
   IonToolbar,
-  IonList,
-  IonItem,
   IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+  useIonViewDidEnter,
+  useIonViewDidLeave,
+  isPlatform,
 } from '@ionic/react';
 import React from 'react';
+import { PluginListenerHandle } from '@capacitor/core';
+import { Browser } from '@capacitor/Browser';
+import { platform } from 'os';
 
 const BrowserPage: React.FC = () => {
+  let finishedHandler: PluginListenerHandle | null = null;
+  let loadedHandler: PluginListenerHandle | null = null;
+
+  function timeout(delay: number) {
+    return new Promise(res => setTimeout(res, delay));
+  }
+
+  const autoCloseAfterDelay = async () => {
+    loadedHandler?.remove();
+    loadedHandler = Browser.addListener('browserPageLoaded', async info => {
+      await timeout(2000);
+      Browser.close();
+      await timeout(200);
+      alert(`The window was closed for you`);
+    });
+  };
+
+  useIonViewDidEnter(() => {
+    Browser.prefetch({ urls: ['https://capacitorjs.com'] });
+    finishedHandler = Browser.addListener('browserFinished', async info => {
+      await timeout(200);
+      alert(`You closed the window`);
+    });
+  });
+
+  useIonViewDidLeave(() => {
+    finishedHandler?.remove();
+    loadedHandler?.remove();
+    finishedHandler = null;
+    loadedHandler = null;
+  });
+
   return (
     <IonPage>
       <IonHeader>
@@ -24,11 +63,37 @@ const BrowserPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonList>
-          <IonItem>
-            <IonButton expand="block">Open browser</IonButton>
-          </IonItem>
-        </IonList>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonButton
+                expand="block"
+                onClick={() => {
+                  loadedHandler?.remove();
+                  loadedHandler = null;
+                  Browser.open({ url: 'https://capacitorjs.com' });
+                }}>
+                Open URL
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          {isPlatform('android') ? (
+            <div />
+          ) : (
+            <IonRow>
+              <IonCol>
+                <IonButton
+                  expand="block"
+                  onClick={() => {
+                    autoCloseAfterDelay();
+                    Browser.open({ url: 'https://capacitorjs.com' });
+                  }}>
+                  Open and then close URL
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          )}
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
