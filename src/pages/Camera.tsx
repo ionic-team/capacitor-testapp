a1c06c02e094eb5b8a00eb96b3ed1d3de71d2454
Ian Keith <iankeith@gmail.com>
2020-11-18 16:44:59 -0500

    Updating camera page.

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -8,7 +8,7 @@ import {
   IonToolbar,
   IonCardContent,
   IonCard,
-  IonButton,
+  IonButton
 } from '@ionic/react';
 import React from 'react';
 import { Camera, CameraOptions, CameraSource } from '@capacitor/camera';
@@ -23,15 +23,34 @@ class CameraPage extends React.Component<{}, CameraPageState> {
     super(props);
     this.state = { filePath: null };
   }
-  addPhoto = async (source: CameraSource) => {
-    const options: CameraOptions = {
-      quality: 100,
-      resultType: 'uri',
-      source: source,
-    };
-    var photo = await Camera.getPhoto(options);
-    this.setState({ filePath: photo.path ?? null });
+
+  addPhoto = async (source: CameraSource, save: boolean = false) => {
+    try {
+      const options: CameraOptions = {
+        quality: 100,
+        resultType: 'uri',
+        source: source,
+        saveToGallery: save,
+        webUseInput: source === 'photos' ? true : false
+      };
+      var photo = await Camera.getPhoto(options);
+      this.setState({ filePath: photo.path ?? (photo.webPath ?? null) });
+    }
+    catch (e) {
+      console.log("failed to get picture", e)
+    }
   };
+
+  checkPermissions = async () => {
+    const permissions = await Camera.checkPermissions();
+    alert(`Permissions are:\ncamera = ${permissions.camera}\nwritePhotos = ${permissions.writePhotos}\nreadPhotos = ${permissions.readPhotos}`);
+  };
+
+  requestPermissions = async () => {
+    const permissions = await Camera.requestPermissions({types: []});
+    alert(`Permissions are:\ncamera = ${permissions.camera}\nwritePhotos = ${permissions.writePhotos}\nreadPhotos = ${permissions.readPhotos}`);
+  };
+
   render() {
     return (
       <IonPage>
@@ -59,13 +78,27 @@ class CameraPage extends React.Component<{}, CameraPageState> {
           <IonCard>
             <IonCardContent>
               <IonButton expand="block" onClick={() => this.addPhoto('camera')}>
-                Use Camera
+                Take Picture
+              </IonButton>
+              <IonButton expand="block" onClick={() => this.addPhoto('camera', true)}>
+                Take Picture and Save
               </IonButton>
               <IonButton expand="block" onClick={() => this.addPhoto('photos')}>
                 Choose Picture
               </IonButton>
               <IonButton expand="block" onClick={() => this.addPhoto('prompt')}>
                 Prompt
+              </IonButton>
+            </IonCardContent>
+          </IonCard>
+
+          <IonCard>
+            <IonCardContent>
+              <IonButton expand="block" onClick={() => this.checkPermissions()}>
+                Check Permissions
+              </IonButton>
+              <IonButton expand="block" onClick={() => this.requestPermissions()}>
+                Request All Permissions
               </IonButton>
             </IonCardContent>
           </IonCard>
