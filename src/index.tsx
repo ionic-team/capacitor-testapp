07922c7bdbc4b0a50d1f02373483f0320e03a4e2
Ian Keith <iankeith@gmail.com>
2020-11-18 16:45:22 -0500

    Adding PWA elements so that camera can work on the web.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { defineCustomElements } from '@ionic/pwa-elements/loader'
+
+// enable PWA elements
+defineCustomElements(window);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
