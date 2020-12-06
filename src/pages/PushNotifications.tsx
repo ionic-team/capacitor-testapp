67c7c263a05cf5d01ecb96213713e6de5a880d2d
Daniel Imhoff <dwieeb@gmail.com>
2020-12-06 15:09:03 -0800

    android working

--- src/pages/PushNotifications.tsx
+++ src/pages/PushNotifications.tsx
@@ -1,3 +1,4 @@
+import { PushNotifications } from '@capacitor/push-notifications';
 import {
   IonButtons,
   IonContent,
@@ -6,10 +7,47 @@ import {
   IonMenuButton,
   IonTitle,
   IonToolbar,
+  useIonViewDidEnter,
 } from '@ionic/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const PushNotifications: React.FC = () => {
+const PushNotificationsPage: React.FC = () => {
+  const ensurePermissions = async () => {
+    try {
+      let { receive } = await PushNotifications.checkPermissions();
+      console.log('PushNotifications receive permission:', receive);
+
+      if (receive === 'prompt') {
+        ({ receive } = await PushNotifications.requestPermissions());
+      }
+
+      if (receive !== 'granted') {
+        throw new Error('User denied permissions!');
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const register = async () => {
+    await PushNotifications.register();
+  };
+
+  useEffect(() => {
+    PushNotifications.addListener('registration', token => {
+      console.error('Registration token:', token);
+    });
+
+    PushNotifications.addListener('registrationError', err => {
+      console.error('Registration error:', err);
+    });
+  }, []);
+
+  useIonViewDidEnter(async () => {
+    await ensurePermissions();
+    await register();
+  });
+
   return (
     <IonPage>
       <IonHeader>
@@ -25,4 +63,4 @@ const PushNotifications: React.FC = () => {
   );
 };
 
-export default PushNotifications;
+export default PushNotificationsPage;
