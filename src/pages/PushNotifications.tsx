c4355836712819f8525e3e668e8a58820d1fe14b
Joseph Pender <joey@ionic.io>
2020-12-18 10:14:04 -0600

    Adding test functionality to push notifications page

--- src/pages/PushNotifications.tsx
+++ src/pages/PushNotifications.tsx
@@ -1,18 +1,38 @@
+import cx from 'classnames';
 import { PushNotifications } from '@capacitor/push-notifications';
+import { PushNotificationDeliveredList } from '@capacitor/push-notifications/dist/esm/definitions';
 import {
   IonButtons,
   IonContent,
   IonHeader,
+  IonButton,
+  IonList,
+  IonItem,
+  IonItemSliding,
+  IonLabel,
   IonPage,
   IonMenuButton,
   IonTitle,
   IonToolbar,
   useIonViewDidEnter,
+  IonItemOptions,
+  IonItemOption,
+  useIonViewWillLeave,
+  IonListHeader,
 } from '@ionic/react';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import './PushNotifications.css';
+import { PermissionState } from '@capacitor/core';
 
 const PushNotificationsPage: React.FC = () => {
-  const ensurePermissions = async () => {
+  const [hasPermission, setHasPermission] = useState<PermissionState>('denied');
+
+  const [
+    notificationList,
+    setNotificationList,
+  ] = useState<PushNotificationDeliveredList | null>(null);
+
+  const ensurePermissions = async (): Promise<PermissionState> => {
     try {
       let { receive } = await PushNotifications.checkPermissions();
       console.log('PushNotifications receive permission:', receive);
@@ -24,28 +44,91 @@ const PushNotificationsPage: React.FC = () => {
       if (receive !== 'granted') {
         throw new Error('User denied permissions!');
       }
+
+      return receive;
     } catch (e) {
+      console.log('permissions error');
       console.error(e);
+
+      return 'denied';
     }
   };
 
   const register = async () => {
-    await PushNotifications.register();
+    try {
+      await PushNotifications.register();
+    } catch (e) {
+      console.log('push notification registration error');
+      console.error(e);
+    }
+  };
+
+  const getDeliveredNotifications = async () => {
+    try {
+      const notificationList = await PushNotifications.getDeliveredNotifications();
+      setNotificationList(notificationList);
+      console.log(notificationList);
+    } catch (e) {
+      console.log('getDeliveredNotifications error');
+      console.error(e);
+    }
+  };
+
+  const removeDeliveredNotifications = async () => {
+    try {
+      await PushNotifications.removeAllDeliveredNotifications();
+      await getDeliveredNotifications();
+    } catch (e) {
+      console.log('removeAllDeliveredNotifications error');
+      console.error(e);
+    }
+  };
+
+  const removeSingleDelieveredNotification = async (
+    list: PushNotificationDeliveredList,
+  ) => {
+    try {
+      await PushNotifications.removeDeliveredNotifications(list);
+      await getDeliveredNotifications();
+    } catch (e) {
+      console.log('removeDeliveredNotifications error');
+      console.error(e);
+    }
   };
 
   useEffect(() => {
     PushNotifications.addListener('registration', token => {
-      console.error('Registration token:', token);
+      console.info('Registration token: ', token);
     });
 
     PushNotifications.addListener('registrationError', err => {
-      console.error('Registration error:', err);
+      console.error('Registration error: ', err);
     });
+
+    PushNotifications.addListener('pushNotificationReceived', notification => {
+      console.log('Push notification received: ', notification);
+    });
+
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      notification => {
+        console.log(
+          'Push notification action performed',
+          notification.actionId,
+          notification.inputValue,
+        );
+      },
+    );
   }, []);
 
   useIonViewDidEnter(async () => {
-    await ensurePermissions();
+    const hasPermission = await ensurePermissions();
+    setHasPermission(hasPermission);
     await register();
+  });
+
+  useIonViewWillLeave(() => {
+    PushNotifications.removeAllListeners();
   });
 
   return (
@@ -58,7 +141,59 @@ const PushNotificationsPage: React.FC = () => {
           <IonTitle>Push Notifications</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent></IonContent>
+      <IonContent>
+        <section>
+          <span
+            className={cx({
+              permissionStatus: true,
+              prompt:
+                hasPermission === 'prompt' ||
+                hasPermission === 'prompt-with-rationale',
+              denied: hasPermission === 'denied',
+              granted: hasPermission === 'granted',
+            })}>
+            Push Notifications Permission{' '}
+            {hasPermission.toString().toUpperCase()}
+          </span>
+          <IonButton onClick={getDeliveredNotifications} expand="block">
+            Get Delivered Notifications
+          </IonButton>
+          <IonButton onClick={removeDeliveredNotifications} expand="block">
+            Remove All Delivered Notifications
+          </IonButton>
+        </section>
+        <br />
+        {notificationList && (
+          <IonList>
+            <IonListHeader>Notifications (swipe to remove)</IonListHeader>
+            {notificationList.notifications.map(notification => {
+              return (
+                <IonItemSliding>
+                  <IonItem>
+                    <IonLabel>
+                      <h2>{notification.title}</h2>
+                      <h3>{notification.subtitle}</h3>
+                      <p>{notification.body}</p>
+                    </IonLabel>
+                  </IonItem>
+                  <IonItemOptions side="end">
+                    <IonItemOption
+                      color="danger"
+                      onClick={() => {
+                        const newList: PushNotificationDeliveredList = {
+                          notifications: [notification],
+                        };
+                        removeSingleDelieveredNotification(newList);
+                      }}>
+                      Remove
+                    </IonItemOption>
+                  </IonItemOptions>
+                </IonItemSliding>
+              );
+            })}
+          </IonList>
+        )}
+      </IonContent>
     </IonPage>
   );
 };
