d720f1f30db113df654797ff9e6c9398e9b2bcff
Daniel Imhoff <dwieeb@gmail.com>
2020-12-26 15:45:07 -0800

    chore(scripts): add update-all script

---
+++ scripts/update-all.mjs
@@ -0,0 +1,52 @@
+import { resolve } from 'path';
+
+import {
+  CORE_PACKAGES,
+  CORE_DEV_PACKAGES,
+  PLUGIN_PACKAGES,
+} from './lib/capacitor.mjs';
+import { execute } from './lib/cli.mjs';
+import { root } from './lib/repo.mjs';
+import { install } from './lib/npm.mjs';
+import {
+  getLatestVersion,
+  setPackageJsonDependencies,
+} from './lib/version.mjs';
+
+execute(async () => {
+  const path = resolve(root, 'package.json');
+
+  const dependencies = {
+    ...Object.fromEntries(
+      await Promise.all(
+        CORE_PACKAGES.map(async project => [
+          `@capacitor/${project}`,
+          `^${await getLatestVersion(`@capacitor/${project}`, 'next')}`,
+        ]),
+      ),
+    ),
+    ...Object.fromEntries(
+      await Promise.all(
+        PLUGIN_PACKAGES.map(async project => [
+          `@capacitor/${project}`,
+          `^${await getLatestVersion(`@capacitor/${project}`)}`,
+        ]),
+      ),
+    ),
+  };
+
+  const devDependencies = {
+    ...Object.fromEntries(
+      await Promise.all(
+        CORE_DEV_PACKAGES.map(async project => [
+          `@capacitor/${project}`,
+          `^${await getLatestVersion(`@capacitor/${project}`, 'next')}`,
+        ]),
+      ),
+    ),
+  };
+
+  await setPackageJsonDependencies(path, dependencies);
+  await setPackageJsonDependencies(path, devDependencies, 'devDependencies');
+  await install();
+});
