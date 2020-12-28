ed810822aadcded21a91ce8d063ab0e6e52f9a1d
Joseph Pender <joey@ionic.io>
2020-12-28 10:09:35 -0600

    adding capacitor.config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,6 +8,9 @@ const config: CapacitorConfig = {
     SplashScreen: {
       launchShowDuration: 0,
     },
+    PushNotifications: {
+      presentationOptions: ["alert", "sound"]
+    }
   },
 };
 
