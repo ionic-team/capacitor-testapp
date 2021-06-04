02bca38074ed5c75d1873fe32fdb1d62a2dc6ab5
Max Lynch <max@ionic.io>
2021-06-03 21:04:46 -0500

    native selectors

--- tests/specs/app.home.spec.ts
+++ tests/specs/app.home.spec.ts
@@ -14,7 +14,12 @@ describe('Home page', () => {
 
     const menu = await $('#menu-button');
     await menu.waitForDisplayed({ timeout: 5000 });
-    await (await expect(menu)).toHaveText('Tab 1');
+    await (await expect(menu)).toExist();
+
+    await menu.click();
+
+    const homeItem = await $('ion-label*=Home');
+    await (await expect(homeItem)).toBeDisplayed();
 
     /**
      * IMPORTANT!!
@@ -27,4 +32,39 @@ describe('Home page', () => {
     WebView.switchToContext(CONTEXT_REF.NATIVE);
   });
 
+  it('should open action sheet page', async () => {
+    WebView.switchToContext(CONTEXT_REF.WEBVIEW);
+    const menu = await $('#menu-button');
+    await menu.waitForDisplayed({ timeout: 5000 });
+    await (await expect(menu)).toExist();
+
+    await menu.click();
+
+    const item = await $('ion-label*=Action Sheet');
+    await item.click();
+
+    const title = await $('ion-title*=Action Sheet');
+    await (await expect(title)).toBeDisplayed();
+    WebView.switchToContext(CONTEXT_REF.NATIVE);
+  });
+
+  it('should open action sheet', async () => {
+    WebView.switchToContext(CONTEXT_REF.WEBVIEW);
+    const button = await $('ion-button=Show Actions');
+    await button.waitForDisplayed({ timeout: 5000 });
+    await (await expect(button)).toBeDisplayed();
+
+    await button.click();
+
+    await driver.pause(4000);
+
+    WebView.switchToContext(CONTEXT_REF.NATIVE);
+
+    //const actionSheet = await $(`~** /XCUIElementTypeSheet[\`label == "Photo Options"\`]`);
+    const actionSheet = await $(`-ios class chain:**/XCUIElementTypeSheet[\`label == "Photo Options"\`]`);
+
+    await actionSheet.waitForDisplayed({ timeout: 5000 });
+    await (await expect(actionSheet)).toBeDisplayed();
+
+  });
 });
