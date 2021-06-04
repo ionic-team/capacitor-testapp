0f8e59ae2cdbca34d5cdb6bdf71865d06de65f5f
Max Lynch <max@ionic.io>
2021-06-04 17:39:46 -0500

    Abstracting away common stuff

--- config/wdio.ios.browser.conf.ts
+++ config/wdio.ios.browser.conf.ts
@@ -4,7 +4,7 @@ import config from './wdio.shared.local.appium.conf';
 // Specs
 // ============
 config.specs = [
-    './tests/specs/**/browser*.spec.ts',
+  './tests/specs/**/browser*.spec.ts',
 ];
 
 // ============
@@ -13,20 +13,20 @@ config.specs = [
 // For all capabilities please check
 // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
 config.capabilities = [
-    {
-        // The defaults you need to have in your config
-        browserName: 'safari',
-        platformName: 'iOS',
-        maxInstances: 1,
-        // For W3C the appium capabilities need to have an extension prefix
-        // This is `appium:` for all Appium Capabilities which can be found here
-        // http://appium.io/docs/en/writing-running-appium/caps/
-        'appium:deviceName': 'iPhone 12',
-        'appium:platformVersion': '14.5',
-        'appium:orientation': 'PORTRAIT',
-        'appium:automationName': 'XCUITest',
-        'appium:newCommandTimeout': 240,
-    },
+  {
+    // The defaults you need to have in your config
+    browserName: 'safari',
+    platformName: 'iOS',
+    maxInstances: 1,
+    // For W3C the appium capabilities need to have an extension prefix
+    // This is `appium:` for all Appium Capabilities which can be found here
+    // http://appium.io/docs/en/writing-running-appium/caps/
+    'appium:deviceName': 'iPhone 12 Pro Max',
+    'appium:platformVersion': '14.5',
+    'appium:orientation': 'PORTRAIT',
+    'appium:automationName': 'XCUITest',
+    'appium:newCommandTimeout': 240,
+  },
 ];
 
 exports.config = config;
--- tests/specs/browser.home.spec.ts
+++ tests/specs/browser.home.spec.ts
@@ -1,42 +1,43 @@
 
 describe('home page', () => {
   beforeEach(async () => {
-    browser.url('home');
-    browser.setWindowSize(375, 812);
+    await browser.setWindowSize(375, 812);
   });
 
-  it('should open menu', async () => {
+  const assertPageChange = async (title: string) => {
+    const pageTitle = await $('ion-title*=Action Sheet');
+    await (await expect(pageTitle)).toBeDisplayed();
+  }
 
-    const menu = await $('#menu-button');
+  const openPage = async (itemText: string) => {
+    // Go back to home
+    await browser.url('/home');
+    const menuButton = await $('#menu-button');
+    await menuButton.waitForDisplayed({ timeout: 5000 });
+    await menuButton.click();
+
+    // Let the menu animate open/closed
+    await browser.pause(300);
+
+    const menu = await $('#inbox-list');
     await menu.waitForDisplayed({ timeout: 5000 });
-    await (await expect(menu)).toExist();
 
-    await menu.click();
+    const linkItem = await menu.$(`ion-item*=${itemText}`);
+    await linkItem.waitForDisplayed({ timeout: 5000 });
+    await linkItem.click();
 
-    const homeItem = await $('ion-label*=Home');
-    await (await expect(homeItem)).toBeDisplayed();
-  });
+    const menuElement = await $('ion-menu');
+    await menuElement.waitForDisplayed({ timeout: 5000, reverse: true });
+  }
 
   it('should open action sheet page', async () => {
-    browser.url('home');
-    browser.setWindowSize(375, 812);
+    await openPage('Action Sheet');
 
-    const menu = await $('#menu-button');
-    await menu.waitForDisplayed({ timeout: 5000 });
-    await (await expect(menu)).toExist();
-
-    await menu.click();
-
-    const item = await $('ion-label*=Action Sheet');
-    await item.click();
-
-    const title = await $('ion-title*=Action Sheet');
-    await (await expect(title)).toBeDisplayed();
+    await assertPageChange('Action Sheet');
   });
 
   it('should open action sheet', async () => {
-    browser.url('action-sheet');
-    browser.setWindowSize(375, 812);
+    await openPage('Action Sheet');
 
     const button = await $('ion-button=Show Actions');
     await button.waitForDisplayed({ timeout: 5000 });
@@ -48,4 +49,10 @@ describe('home page', () => {
     await pwaActionSheet.waitForDisplayed({ timeout: 5000 });
     await (await expect(pwaActionSheet)).toBeDisplayed();
   });
+
+  /*
+  it('should open app page', async () => {
+    await openPage('App');
+  });
+  */
 });
