090ee60964b003db4413b4fc789c87428b4370ff
Max Lynch <max@ionic.io>
2021-06-20 19:07:15 -0500

    Build

--- build.ios.sh
+++ build.ios.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # ionic capacitor sync
 rm -rf app.xcarchive
+xcodebuild clean -workspace ios/App/App.xcworkspace -scheme App
 xcodebuild archive -workspace ios/App/App.xcworkspace -scheme App -sdk "iphonesimulator" -destination "generic/platform=iOS Simulator" -configuration Release -archivePath app/
 zip -r app/App.zip app.xcarchive/Products/Applications/App.app
