511a0523e40bd15569d2ffa2cd1e2b3abb407138
Nicholas Hyatt <nicholaspaulhyatt@gmail.com>
2021-10-27 13:47:19 -0500

    add appflow build stuffs

---
+++ run-appium-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+if [ "$CI_PLATFORM" == "ios" ]; then
+        echo "Running iOS Build"
+        npm i -g appium@latest
+        npm run build
+        npx cap sync ios
+        npm run e2e:build ios:simulator
+        xcrun simctl list
+        xcrun simctl boot "iPhone 12 Pro"
+        npm run e2e:run ios:simulator
+fi
+
+if [ "$CI_PLATFORM" == "android" ]; then
+        echo "Running Android Build"
+fi
+
+if [ "$CI_PLATFORM" == "web" ]; then
+        echo "Running Web Build"
+fi
