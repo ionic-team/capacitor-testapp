962ad31d85dd2401dddb83ee370d99d59a872ff6
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2022-09-28 11:03:38 -0400

    chore: fix tests

--- android/app/src/main/java/com/capacitorjs/app/testapp/MainActivity.java
+++ android/app/src/main/java/com/capacitorjs/app/testapp/MainActivity.java
@@ -2,4 +2,4 @@ package com.capacitorjs.app.testapp;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity { }
+public class MainActivity extends BridgeActivity {}
