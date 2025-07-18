e68cc4602c9104b7575daa7110eb396971eabd60
Kepa Totorica <kepa.baum.totorica@outsystems.com>
2025-07-18 10:40:37 -0600

    added a comment with a description (#726)

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -160,6 +160,15 @@ const AppPage: React.FC = () => {
         <IonButton expand="block" onClick={failCall}>
           Test Failing Call
         </IonButton>
+        <IonButton expand="block" href="https://flems.io/#0=N4IgzgpgNhDGAuEAmIBcIB0ALeBbKIANCAGYCWMYaA2qAHYCGuEamO+RIsA9nYn6wA8WAIwA+ADp1BDAARYAThBIBeCSDJ8IfVAHpdWbswDEASS18A3GFhYIzFT1wZYDAA4NYZeNwUArMAx3NwxEMHhgyw9YAGsGAHMIRyMXd09vXwCgtxCwiJzLT3gyXhUGOiQFbjIkDE1+eCCEEroMADVTAFEAdUttJEt1STpZWQAVCHDUWQB5N21ZAEEABWWpQV0GYY3RMU5IGGbeKnQAJlQRADYQAF9CeiYWdAwAzh4LeCE5RWU1LhTXNEMv5AsFQpN8m49AYjBAAPwkKq4FRICAQNxQTQxIZjToAZTGGy2+2gcGKx1YAAZUJcAOy3e4gRjMVguMBUYjvBqsW4AXRuQA">
+        {/* If the link above ever expires, this is the HTML to test opening an intent for this app.
+        <h1>
+        <a href="intent://home#Intent;scheme=com.capacitorjs.app.testapp;package=com.capacitorjs.app.testapp;action=android.intent.action.VIEW;end;">
+          Test: Open APP
+        </a>
+        </h1> */}
+          Test Intents
+        </IonButton>
         <p>
           <a href="tel:212-549-2543">Telephone Test</a>
           <a href="mailto:name@email.com">Email Test</a>
